3615f16a8fae1189bf7a379410ed2e6907d04e2f
buithithuyquynh1993 <buithithuyquynh1993@gmail.com>
2014-11-01 20:52:42 +0700

    Them file moi

---
+++ QuanLiThuVien/QuanLiThuVien/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLiThuVien.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DanhSachSachDat()
+        {
+            return View();
+        }
+        public ActionResult ChinhSuaDatMuon()
+        {
+            return View();
+        }
+        public ActionResult GiaHanMuonSach()
+        {
+            return View();
+        }
+        public ActionResult TimKiemSach()
+        {
+            return View();
+        }
+
+    }
+}
