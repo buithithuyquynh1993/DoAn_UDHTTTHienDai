9a6f832edf641ffbd385162a225632389ad30520
tuyetphale993 <tuyetphale993@gmail.com>
2014-11-04 20:41:57 +0700

    - add get list borrows
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

---
+++ QuanLiThuVien/QuanLiThuVien/Controllers/SachController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLiThuVien.Models;
+
+namespace QuanLiThuVien.Controllers
+{
+    public class SachController : Controller
+    {
+        //
+        // GET: /Sach/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getListBorrowers() {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            var result = from t in data.View_Borrowers select t;
+            return View(result);
+        }
+    }
+}
