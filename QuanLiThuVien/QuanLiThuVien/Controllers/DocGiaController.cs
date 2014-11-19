62641dcb17fa3d630d7879b638e733c3b94de6de
buithithuyquynh1993 <buithithuyquynh1993@gmail.com>
2014-11-19 16:40:50 +0700

    lần 2

---
+++ QuanLiThuVien/QuanLiThuVien/Controllers/DocGiaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLiThuVien.Models;
+namespace QuanLiThuVien.Controllers
+{
+    public class DocGiaController : Controller
+    {
+        //
+        // GET: /DocGia/
+
+        public ActionResult save(FormCollection f)
+        {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            DOCGIA p = new DOCGIA();
+            p.Hoten = f["Hoten"];
+            p.MHV_MSSV = f["MHV_MSSV"];
+            p.Email = f["Email"];
+            p.CMND = f["CMND"];
+            p.DiaChi = f["DiaChi"];
+            p.Truong = f["Truong"];
+            p.Khoa = f["Khoa"];
+            data.DOCGIAs.Add(p);
+            data.SaveChanges();
+           // data.SaveChanges();
+            return RedirectToAction("add");
+        }
+
+        public ActionResult add()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuanLiThuVien/QuanLiThuVien/Controllers/PhongController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLiThuVien.Models;
+
+namespace QuanLiThuVien.Controllers
+{
+    public class PhongController : Controller
+    {
+        //
+        // GET: /Phong/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetListBorrowedRoom()
+        {
+            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            var query = (from ls in data.LICHSUMUONPHONGs
+                        join phong in data.PHONGs on ls.IDPhong equals phong.ID
+                        join docgia in data.DOCGIAs on ls.IDDocGia equals docgia.ID
+                        select ls);
+                        //select new {HoTen = docgia.Hoten,
+                        //            tgmuon = ls.ThoiGianMuon,
+                        //            tgtra = ls.ThoiGianTra,
+                        //            phong = phong.ID});
+            ViewBag.data = query.ToArray();
+            return View();
+        }
+        public ActionResult BorrowedRoom()
+        {
+            return View();
+        }
+    }
+}
