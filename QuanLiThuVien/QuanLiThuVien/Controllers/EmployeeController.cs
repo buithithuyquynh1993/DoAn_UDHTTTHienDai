21f475f346a3945c23a0465d03bcce16e03139fe
tuyetphale993 <tuyetphale993@gmail.com>
2014-12-16 23:07:56 +0700

    - add some Quynh's function
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- QuanLiThuVien/QuanLiThuVien/Controllers/EmployeeController.cs
+++ QuanLiThuVien/QuanLiThuVien/Controllers/EmployeeController.cs
@@ -12,6 +12,108 @@ namespace QuanLiThuVien.Controllers
         //
         // GET: /Employee/  
         QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+        public ActionResult GetListBorrowedRoom(int? page, string key, string keydate)
+        {
+            try
+            {
+                DateTime now = DateTime.Today;
+                int pageSize = 5;
+                int pageNumber = (page ?? 1);
+                string keyword;
+                string temp;
+                if (page == null)
+                {
+                    keyword = @Request["keyword"];
+                    temp = @Request["date"];
+                }
+                else
+                {
+                    keyword = key;
+                    temp = keydate;
+                }
+                ViewBag.keyword = keyword;
+                ViewBag.date = temp;
+                if ((keyword == null && temp == null) || (keyword == "" && temp == ""))
+                {
+                    page = 1;
+                    var query = (from ls in data.LICHSUMUONPHONGs
+                                 join phong in data.PHONGs on ls.IDPhong equals phong.ID
+                                 join docgia in data.DOCGIAs on ls.IDDocGia equals docgia.ID
+                                 where ls.ThoiGianMuon > now
+                                 select new
+                                 {
+                                     HoTen = docgia.Hoten,
+                                     tgmuon = ls.ThoiGianMuon,
+                                     tgtra = ls.ThoiGianTra,
+                                     phong = phong.ID
+                                 }).OrderByDescending(a => a.tgmuon);
+                    return View("GetListBorrowedRoom", query.ToPagedList(pageNumber, pageSize));
+                }
+                else
+                {
+                    DateTime date = DateTime.Today;
+                    if (temp != "" && temp != null)
+                    {
+                        string[] CatChuoi = temp.Split('/');
+                        string ngaymuon = CatChuoi[1] + "/" + CatChuoi[0] + "/" + CatChuoi[2];
+                        date = DateTime.Parse(ngaymuon);
+                    }
+                    if ((keyword != "" && temp != "") && (keyword != null && temp != null))
+                    {
+                        var query = (from ls in data.LICHSUMUONPHONGs
+                                     join phong in data.PHONGs on ls.IDPhong equals phong.ID
+                                     join docgia in data.DOCGIAs on ls.IDDocGia equals docgia.ID
+                                     where docgia.Hoten.Contains(@keyword)
+                                     && DateTime.Compare(ls.ThoiGianMuon, date).Equals(1)
+                                     select new
+                                     {
+                                         HoTen = docgia.Hoten,
+                                         tgmuon = ls.ThoiGianMuon,
+                                         tgtra = ls.ThoiGianTra,
+                                         phong = phong.ID
+                                     }).OrderBy(a => a.tgmuon);
+                        return View("GetListBorrowedRoom", query.ToPagedList(pageNumber, pageSize));
+                    }
+                    if (keyword != "" && keyword != null)
+                    {
+                        var query = (from ls in data.LICHSUMUONPHONGs
+                                     join phong in data.PHONGs on ls.IDPhong equals phong.ID
+                                     join docgia in data.DOCGIAs on ls.IDDocGia equals docgia.ID
+                                     where docgia.Hoten.Contains(@keyword)
+                                     select new
+                                     {
+                                         HoTen = docgia.Hoten,
+                                         tgmuon = ls.ThoiGianMuon,
+                                         tgtra = ls.ThoiGianTra,
+                                         phong = phong.ID
+                                     }).OrderByDescending(a => a.tgmuon);
+                        return View("GetListBorrowedRoom", query.ToPagedList(pageNumber, pageSize));
+                    }
+                    if (temp != "" && temp != null)
+                    {
+                        var query = (from ls in data.LICHSUMUONPHONGs
+                                     join phong in data.PHONGs on ls.IDPhong equals phong.ID
+                                     join docgia in data.DOCGIAs on ls.IDDocGia equals docgia.ID
+                                     where DateTime.Compare(ls.ThoiGianMuon, date).Equals(1)
+                                     select new
+                                     {
+                                         HoTen = docgia.Hoten,
+                                         tgmuon = ls.ThoiGianMuon,
+                                         tgtra = ls.ThoiGianTra,
+                                         phong = phong.ID
+                                     }).OrderBy(a => a.tgmuon);
+                        return View("GetListBorrowedRoom", query.ToPagedList(pageNumber, pageSize));
+                    }
+
+                }
+                return View();
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
         public ActionResult BorrowedBook()
         {
             return View();
@@ -41,7 +143,7 @@ namespace QuanLiThuVien.Controllers
         }
         public ActionResult SaveBorrowedRoom()
         {
-            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            //QuanLyThuVienEntities data = new QuanLyThuVienEntities();
             LICHSUMUONPHONG ls = new LICHSUMUONPHONG();
             ls.IDDocGia = int.Parse(@Request["IDDocGia"].ToString());
             ls.IDPhong = int.Parse(@Request["IDPhong"].ToString());
@@ -59,7 +161,7 @@ namespace QuanLiThuVien.Controllers
             try
             {
                 proc_layTTMuonSach_Result KQ = new proc_layTTMuonSach_Result();
-                QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+                //QuanLyThuVienEntities data = new QuanLyThuVienEntities();
                 String maDocGia = Request.Form["madocgia"];
                 String maSach = Request.Form["masach"];
                 var query = data.proc_layTTMuonSach(maSach);
@@ -71,7 +173,6 @@ namespace QuanLiThuVien.Controllers
                 return View("TraSach", null);
             }
         }
-        //public bool thuchienTraSach(int ID, int? phat)
         public bool thuchienTraSach(FormCollection f){
             try
             {
@@ -79,7 +180,7 @@ namespace QuanLiThuVien.Controllers
                 int phat = int.Parse(f["tongphiphat"]);
                 String lido = (f["lidophat"]);
                 
-                QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+                //QuanLyThuVienEntities data = new QuanLyThuVienEntities();
                 THONGTINMUONTRA result = (from tt in data.THONGTINMUONTRAs where tt.ID == ID select tt).First();
                 result.NgayTra = DateTime.Now;
                 //if (phat != null)
@@ -106,7 +207,7 @@ namespace QuanLiThuVien.Controllers
         #region xem danh sách phiếu góp ý
         public ActionResult xemDSPhieuGopY()
         {
-            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            //QuanLyThuVienEntities data = new QuanLyThuVienEntities();
             var query = from p in data.THUGOPies where p.DaXem == false select p;
             return View(query);
         }
@@ -118,7 +219,7 @@ namespace QuanLiThuVien.Controllers
             {
                 var temp = f["checkGopY"].Replace(",false", "");
                 string[] arrtem = temp.Split(',');
-                QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+                //QuanLyThuVienEntities data = new QuanLyThuVienEntities();
                 foreach (var i in arrtem)
                 {
                     int iId = int.Parse(i);
@@ -137,7 +238,7 @@ namespace QuanLiThuVien.Controllers
 
         public ActionResult layDSNguoiMuon()
         {
-            QuanLyThuVienEntities data = new QuanLyThuVienEntities();
+            //QuanLyThuVienEntities data = new QuanLyThuVienEntities();
             var result = from t in data.VIEW_BORROWERS select t;
             var tem = result.GetType();
             return View(result);
